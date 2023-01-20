f87ea0e226bb3e917e3cbfc9f0731f152b2a62d3
iljuanss <55993599+iljuanss@users.noreply.github.com>
2023-01-20 13:04:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello world\n');
-}).listen(3000, '127.0.0.1');
+}).listen(3000, '172.174.38.191');
 console.log('Server running at 127.0.0.1:3000');

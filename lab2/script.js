f937d8f390e46487a2224ed40d30b20a3a3119c0
iljuanss <ilhwansong0930@gmail.com>
2023-01-27 00:40:21 -0500

    weather

--- lab2/script.js
+++ lab2/script.js
@@ -1,6 +1,7 @@
 function getLocation() {
 
   if (navigator.geolocation) {
+    console.log("Hi");
     navigator.geolocation.getCurrentPosition(showLoc, showError);
   }else{
   }
@@ -11,32 +12,6 @@ function showLoc(pos) {
   var lat = pos.coords.latitude;
   var long = pos.coords.longitude;
   weather.fetchWeather(lat, long);
-  // var xhttp = new XMLHttpRequest();
-
-  //   xhttp.onreadystatechange = function () {
-  //     if (this.readyState == 4 && this.status == 200) {
-
-  //       var jsonObj = JSON.parse(this.responseText);
-
-  //       var temperature = (1.8 * (jsonObj.main.temp - 273) + 32).toFixed(0) + "°F";
-  //       var feelsLike = "<strong>feels like:</strong> " + (1.8 * (jsonObj.main.feels_like - 273) + 32).toFixed(0) + "°F";
-  //       var pressure = jsonObj.main.pressure + " hPa";
-  //       var humidity = jsonObj.main.humidity + "%";
-  //       var wind = (jsonObj.wind.speed * 2.237).toFixed(2) + " mph";
-
-  //       document.getElementById("loc").innerHTML = jsonObj.name;
-  //       document.getElementById("temp").innerHTML = temperature;
-  //       document.getElementById("weather").innerHTML = jsonObj.weather[0].description + "<br><br>";
-  //       document.getElementById("feels").innerHTML = feelsLike;
-  //       document.getElementById("hum").innerHTML = humidity;
-  //       document.getElementById("pre").innerHTML = pressure;
-  //       document.getElementById("speed").innerHTML = wind;
-  //     }
-  //   };
-
-  //   xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=269c0626376ca1c96a1e100335085499", true);
-
-  //   xhttp.send();
 }
 
 let weather = {
@@ -46,12 +21,15 @@ let weather = {
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
-      const {name} = data.name; //I am extracting data from the json file now
+      var name = data.name; //I am extracting data from the json file now
+      let icon = data.weather[0].icon;
+      console.log(icon);
+      icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
       // const {icon} = data.cion
       // const{icon, description} = data.weather[0]; //the data under weather
       // const{temp,humidity} = data.main;
       // const{speed} = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".city").innerHTML = "Weather in " + name;
       // document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
       // document.querySelector(".description").innerText = description;
       // document.querySelector(".temp").innerText = temp + "°C";

51e71d6d0bcfee03413279868cde9f332a063b8f
iljuanss <ilhwansong0930@gmail.com>
2023-01-26 23:26:26 -0500

    help

--- lab2/script.js
+++ lab2/script.js
@@ -46,20 +46,18 @@ let weather = {
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
-      const {name} = data; //I am extracting data from the json file now
-      const{icon, description} = data.weather[0]; //the data under weather
-      const{temp,humidity} = data.main;
-      const{speed} = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
+      const {name} = data.name; //I am extracting data from the json file now
+      // const {icon} = data.cion
+      // const{icon, description} = data.weather[0]; //the data under weather
+      // const{temp,humidity} = data.main;
+      // const{speed} = data.wind;
+      // document.querySelector(".city").innerText = "Weather in " + name;
+      // document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+      // document.querySelector(".description").innerText = description;
+      // document.querySelector(".temp").innerText = temp + "°C";
+      // document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
+      // document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
   },
-  search:function (){
-      this.fetchWeather(document.querySelector(".search-bar").value);
-  }
 };
 
 

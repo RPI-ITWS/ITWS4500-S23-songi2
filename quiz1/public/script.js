306924626c876d4947f8baec57d227c0db10d357
iljuanss <ilhwansong0930@gmail.com>
2023-02-28 12:59:54 -0500

    ㅇ

--- quiz1/public/script.js
+++ quiz1/public/script.js
@@ -1,24 +1,15 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-if (searchButton) {searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  search(inputValue);
-});
-}
-
-
-
-function search(loc){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200) {
-            var jsonObj = JSON.parse(this.responseText);
-           console.log(jsonObj);
-            document.getElementById("school").innerHTML = jsonObj[0].name;
-            document.getElementById("country").innerHTML = jsonObj[0].country;
-
-        }
+$('document').ready(() => {
+    $('#signup').on('submit', handleSignup)
+    function handleSignup (e) {
+      e.preventDefault()
+      const options = {
+        method: $(this).attr('method'),
+        url: $(this).attr('action'),
+        data: $(this).serialize()
+      }
+      $.ajax(options).done(response => {
+        alert(JSON.stringify(response))
+      })
     }
-    xhttp.open("GET", "https://smolae.eastus.cloudapp.azure.com/node/api");
-    xhttp.send();
-}
+})
+
--- quiz1/server.js
+++ quiz1/server.js
@@ -4,29 +4,22 @@ var bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.static('./public'));
-// app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-// // app.get('/', function(req, res){
-// //   res.sendFile(__dirname + '/index.html');
-// // });
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-// app.post('/addUser',function (req, res){
-//     const user = {
-//       name: req.body.name,
-//     }
-//     fetch('http://universities.hipolabs.com/search?name=' + req.body)
-//     .then(data => data.json())
-//     console.log(data)
-//     console.log(user)
-//     res.send(user)
-//   });
-app.get('/api', (req, res)=> {  
+app.post('/addUser',function (req, res){
+    const user = {
+      name: req.body.name,
+    }
     fetch('http://universities.hipolabs.com/search?name=' + req.body)
     .then(data => data.json())
-    .then(success => res.json(success));
-});
-
-
+    console.log(data)
+    console.log(user)
+    res.send(user)
+  });
 
 // app.get('/addUser', function (req, res){
 //   request(options, function (error, response, body) {

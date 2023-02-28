7681c7e96a4a1dcb3e7348ede445e6485e791cd3
iljuanss <ilhwansong0930@gmail.com>
2023-02-28 13:01:34 -0500

    Update server.js

--- quiz1/server.js
+++ quiz1/server.js
@@ -14,23 +14,21 @@ app.post('/addUser',function (req, res){
     const user = {
       name: req.body.name,
     }
-    fetch('http://universities.hipolabs.com/search?name=' + req.body)
-    .then(data => data.json())
-    console.log(data)
     console.log(user)
     res.send(user)
   });
 
-// app.get('/addUser', function (req, res){
-//   request(options, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       parsed = JSON.parse(body);
-//     }
-//     res.json(parsed);
-//     console.log(parsed);
-//   });
+app.get('/addUser', function (req, res){
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      parsed = JSON.parse(body);
+    }
+    res.json(parsed);
+    console.log(parsed);
 
-// })
+  });
+
+})
 
 // app.get('/api', (req, res)=> {  
 //     fetch('http://universities.hipolabs.com/search?name=' + req.body)

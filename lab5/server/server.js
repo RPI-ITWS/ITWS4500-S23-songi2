7365e46911f4830b6e5761b02592adda3bd7fc0d
iljuanss <ilhwansong0930@gmail.com>
2023-03-26 12:21:06 -0400

    update server.js

--- lab5/server/server.js
+++ lab5/server/server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // This is to have the port can connect to data from other port
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build/static')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));

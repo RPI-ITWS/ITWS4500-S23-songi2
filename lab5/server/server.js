d9d17c063c2bffaabea83a9256b7721a8da819fb
iljuanss <ilhwansong0930@gmail.com>
2023-03-26 12:01:22 -0400

    update server.js

--- lab5/server/server.js
+++ lab5/server/server.js
@@ -9,9 +9,7 @@ app.use(bodyParser.json());
 
 app.use('/static', express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 
 // Connecting Mong
 const MongoClient = require('mongodb').MongoClient;

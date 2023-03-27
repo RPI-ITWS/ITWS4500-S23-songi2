2fa514b826f7917fb5bf0c2e20879fae69531ce5
iljuanss <ilhwansong0930@gmail.com>
2023-03-27 19:58:46 -0400

    update server.js

--- lab5/server/server.js
+++ lab5/server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static('../client/build'));
+app.use('/node', express.static(path.join(__dirname, '../client/build')));
 // app.use(express.static(path.join(__dirname, '../client/build')));
 // app.use('/node', express.static(path.join(__dirname, '../client/build')));
 

b71bff988f3c09fff18fd6bca719bc06502eb0e0
iljuanss <ilhwansong0930@gmail.com>
2023-03-27 13:34:46 -0400

    Update server.js

--- lab5/server/server.js
+++ lab5/server/server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // This is to have the port can connect to data from other port
 app.use(bodyParser.json());
 
-// app.use('/static'. express.static(path.join(__dirname, '../client/build')));
+app.use('/static'. express.static(path.join(__dirname, '../client/build')));
 // app.use('/node', express.static(path.join(__dirname, '../client/build')));
 
 // Connecting Mong
